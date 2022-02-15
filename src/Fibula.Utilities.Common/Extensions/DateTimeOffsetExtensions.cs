4aa364dc03752d5fa9a6a34aabe2876907af0470
Jose Nunez de Caceres <jlnunez89@gmail.com>
2022-02-14 20:20:30 -0800

    added DateTimeOffset.Round method

---
+++ src/Fibula.Utilities.Common/Extensions/DateTimeOffsetExtensions.cs
@@ -0,0 +1,40 @@
+﻿// -----------------------------------------------------------------
+// <copyright file="TimeSpanExtensions.cs" company="2Dudes">
+// Copyright (c) | Jose L. Nunez de Caceres et al.
+// https://linkedin.com/in/nunezdecaceres
+//
+// All Rights Reserved.
+//
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+// </copyright>
+// -----------------------------------------------------------------
+
+namespace Fibula.Utilities.Common.Extensions
+{
+    using System;
+
+    /// <summary>
+    /// Static class that contains helper methods for <see cref="DateTimeOffset"/>s.
+    /// </summary>
+    public static class DateTimeOffsetExtensions
+    {
+        /// <summary>
+        /// Rounds the given datetime by a given timespan.
+        /// </summary>
+        /// <param name="dateTime">The datetime to round.</param>
+        /// <param name="roundBy">The time span to round by.</param>
+        /// <returns>The rounded time.</returns>
+        public static DateTimeOffset Round(this DateTimeOffset dateTime, TimeSpan roundBy)
+        {
+            long remainderTicks = dateTime.Ticks % roundBy.Ticks;
+            if (remainderTicks < roundBy.Ticks / 2)
+            {
+                // round down
+                return dateTime.AddTicks(-remainderTicks);
+            }
+
+            // round up
+            return dateTime.AddTicks(roundBy.Ticks - remainderTicks);
+        }
+    }
+}
